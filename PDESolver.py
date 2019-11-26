0019660567ad19b14b87047eafed4215265e4186
sabs-r3 <spamspambrennan@gmail.com>
2019-11-26 16:46:30 +0000

    This code does not work. It is not even close. We can start on Monday.

--- PDESolver.py
+++ PDESolver.py
@@ -6,6 +6,26 @@ import matplotlib.pyplot as plt
 def func(x):
     return np.exp(-x)
 
+def divergence(x):
+    grad = np.gradient(x)
+    xs = np.gradient(grad[0])[0]
+    ys = np.gradient(grad[1])[1]
+    return np.sqrt(xs**2 + ys**2)
+
+
+# Writing diff equation
+# THIS IS NOT EVEN CLOSE TO WORKING!!!!!!!!!
+def spread_once(xs, g):
+    grad = np.gradient(xs)
+    mag_grad = grad[0]**2 + grad[1]**2
+    g_at_each_point = g(mag_grad)
+    grad_of_g = np.gradient(g_at_each_point)
+    return g_at_each_point @ divergence(xs) + grad @ grad_of_g
+
+
+
+
+    return 0
 
 im = cv2.imread("dog.jpg")
 print(type(im))
