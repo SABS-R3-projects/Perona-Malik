6347485b34de236745f9b1629ff01162562908ec
sabs-r3 <spamspambrennan@gmail.com>
2019-11-28 16:25:14 +0000

    Perona-Malik does not work

--- PDESolver.py
+++ PDESolver.py
@@ -3,7 +3,7 @@ import cv2
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-k = 100
+k = 0.01
 
 # Smooth,positive, non-increasing function g:
 def func(x):
@@ -25,22 +25,25 @@ def prepare_to_plot(xs):
 
 
 # Advance the matrix by one timestep (dt)
-def spread_once(xs, g = func, dt=0.01):
+def spread_once(xs, g = func, dt=0.02):
     grad = np.gradient(xs)
     magnitude_of_grad = np.sqrt(grad[0] ** 2 + grad[1] ** 2)
     g_at_each_point = g(magnitude_of_grad)
-    #grad_of_g = np.gradient(g_at_each_point)
-    return xs + dt * (g_at_each_point * divergence(xs)) #+ grad[0] * grad_of_g[0] + grad[1] * grad_of_g[1])
+    grad_of_g = np.gradient(g_at_each_point)
+    xs[1:-1,1:-1] = xs[1:-1,1:-1] + dt*(divergence(xs)[1:-1,1:-1]*g_at_each_point[1:-1,1:-1]
+                                        + grad[0][1:-1,1:-1] * grad_of_g[0][1:-1,1:-1]
+                                        + grad[1][1:-1,1:-1] * grad_of_g[1][1:-1,1:-1])
+    return xs
 
 
 if __name__ == '__main__':
 
     # Store Image as a numpy array:
-    im = cv2.imread("Apple.jpg")
+    im = cv2.imread("Smile.png")
     xs = im.copy()
 
     # Add noise to the image:
-    noise = np.random.randint(-50, 50, xs.shape)
+    noise = np.random.randint(-10, 10, xs.shape)
     xs = xs + noise
 
     plt.imshow(xs)
@@ -52,7 +55,7 @@ if __name__ == '__main__':
     #ims.append([plt.imshow(xs, animated=True)])
     errs.append(np.mean((im-xs)**2))
 
-    for i in range(200):
+    for i in range(100):
         xs[:, :, 0] = spread_once(xs[:, :, 0], func)
         xs[:, :, 1] = spread_once(xs[:, :, 1], func)
         xs[:, :, 2] = spread_once(xs[:, :, 2], func)
