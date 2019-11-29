5aeda7c1fa7b3006b5fcaa4eac2a8a67320ef3d6
sabs-r3 <itaimuzh@gmail.com>
2019-11-29 11:17:52 +0000

    Changed the order of the color channel to get a red apple instead of a blue one

--- Anisetropic_diff.py
+++ Anisetropic_diff.py
@@ -1,52 +1,59 @@
 import numpy as np
 import cv2
+
 import matplotlib.pyplot as plt
 from medpy.filter.smoothing import anisotropic_diffusion
-#from skimage import data, img_as_float
-from skimage.metrics import structural_similarity as ssim
+from skimage.measure import compare_ssim as ssim
+
 
 img = np.random.uniform(size=(32,32))
-img_filtered = anisotropic_diffusion(img)
 im = cv2.imread("Apple.jpg")
-
+im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
 xs = im.copy()
 
 def prepare_to_plot(xs):
+    """
+    :param xs: input array of the image
+    :return: Optimized array within the range of 0 to 255
+    """
     ranged = np.amax(xs) - np.amin(xs)
     return ((xs + np.amin(xs)) / ranged * 255).astype(int)
 
-# Add noise to the image:
-def add_noise():
-    xs = im.copy()
-    noise = np.random.randint(-50, 50, xs.shape)
+def add_noise(xs):
+    """
+    A method used to add noise to an image
+    :return: An array corresponding to a noisy image
+    """
+    noise = np.random.randint(-20, 20, xs.shape)
     xs = xs + noise
     return prepare_to_plot(xs)
 
-def filter():
-    img_filtered = anisotropic_diffusion(xs, niter = 45, option=3)
+def filter(xs):
+    """
+    Method to filter noise out of an image.
+    :param xs: an array corresponding to a noisy image
+    :return: An array corresponding to a cleaner image
+    """
+    img_filtered = anisotropic_diffusion(xs, niter = 20, option=1)
+    #img_filtered = anisotropic_diffusion(xs, niter=5, option=1)
     return prepare_to_plot(img_filtered)
 
-def mse(imageA, imageB):
-    # the 'Mean Squared Error' between the two images is the
-    # sum of the squared difference between the two images;
-    # NOTE: the two images must have the same dimension
-    err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
-    err /= float(imageA.shape[0] * imageA.shape[1])
-
-    # return the MSE, the lower the error, the more "similar"
-    # the two images are
-    return err
-
-def plot_images():
-    noisy_image = add_noise()
-    filtered_image = filter()
-    original_image = prepare_to_plot(im)
-    #print(mse(original_image, filtered_image))
-   # print(mse(noisy_image, filtered_image))
-    #ssim_noise = ssim(original_image, filtered_image,data_range=filtered_image.max() - filtered_image.min())
-   # print(ssim_noise)
+def plot_images(xs):
+    """
+    Method used to display a panel of images before and after noise removal
+    :param xs: An array corresponding to a clean image to be imported
+    :return: A panel of the original image, image with noise added, and an image with the noise filtered
+    """
+    noisy_image = add_noise(xs)
+    filtered_image = filter(noisy_image)
+    original_image = prepare_to_plot(xs)
+    #ssim calculates the structural similarity index between the noisy and the filtered image. The less the similarity index, the cleaner the filtered image
+    ssim_noise = ssim(noisy_image, filtered_image,
+                      multichannel=True,data_range=filtered_image.max()
+                                                   - filtered_image.min())
+    print(ssim_noise)
     plt.subplot(1, 3, 1)
-    plt.imshow(original_image)
+    plt.imshow( original_image)
     plt.xlabel("Original Image")
     plt.subplot(1, 3, 2)
     plt.imshow(noisy_image)
@@ -56,7 +63,7 @@ def plot_images():
     plt.xlabel("Filtered Image")
     plt.show()
 
-plot_images()
+plot_images(xs)
 
 
 
--- PDESolver.py
+++ PDESolver.py
@@ -2,12 +2,14 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-
+from scipy import optimize
+import cma
+from skimage.measure import compare_ssim as ssim
 
 k = 0.01
 
 # Smooth,positive, non-increasing function g:
-def func(x):
+def func(x, k=0.01):
     return np.exp(-(x*k)**2)
 
 
@@ -26,10 +28,10 @@ def prepare_to_plot(xs):
 
 
 # Advance the matrix by one timestep (dt)
-def spread_once(xs, g = func, dt=0.02):
+def spread_once(xs, k=0.01, dt=0.02):
     grad = np.gradient(xs)
     magnitude_of_grad = np.sqrt(grad[0] ** 2 + grad[1] ** 2)
-    g_at_each_point = g(magnitude_of_grad)
+    g_at_each_point = np.exp(-(magnitude_of_grad)*k)
     grad_of_g = np.gradient(g_at_each_point)
     xs[1:-1,1:-1] = xs[1:-1,1:-1] + dt*(divergence(xs)[1:-1,1:-1]*g_at_each_point[1:-1,1:-1]
                                         + grad[0][1:-1,1:-1] * grad_of_g[0][1:-1,1:-1]
@@ -37,39 +39,37 @@ def spread_once(xs, g = func, dt=0.02):
     return xs
 
 
-if __name__ == '__main__':
-
-    # Store Image as a numpy array:
-    im = cv2.imread("Smile.png")
+def add_noise():
+    im = cv2.imread("Smily.png")
     xs = im.copy()
-
-    # Add noise to the image:
     noise = np.random.randint(-10, 10, xs.shape)
     xs = xs + noise
+    return xs
 
-    plt.imshow(xs)
-    plt.show()
-
-    fig = plt.figure()
-    #ims = []
-    errs = []
-    #ims.append([plt.imshow(xs, animated=True)])
-    errs.append(np.mean((im-xs)**2))
-
+def smoothing_function(k=0.01, dt =0.02):
+    xs = add_noise()
     for i in range(100):
-        xs[:, :, 0] = spread_once(xs[:, :, 0], func)
-        xs[:, :, 1] = spread_once(xs[:, :, 1], func)
-        xs[:, :, 2] = spread_once(xs[:, :, 2], func)
-        #image = plt.imshow(xs, animated=True)
-        #ims.append([image])
-        errs.append(np.mean((im - xs) ** 2))
+        print(i)
+        xs[:, :, 0] = spread_once(xs[:, :, 0],k, dt)
+        xs[:, :, 1] = spread_once(xs[:, :, 1],k, dt)
+        xs[:, :, 2] = spread_once(xs[:, :, 2],k, dt)
+    return xs
 
-    #ani = animation.ArtistAnimation(fig, ims, interval=20, blit=True, repeat_delay=100)
-    #plt.show()
 
-    plt.plot(errs)
-    plt.show()
+def image_difference(k_array):
+    noisy_image = add_noise()
+    filtered_image = smoothing_function(k_array[0], k_array[1])
+    difference = 1 - ssim(noisy_image, filtered_image, multichannel=True,data_range=filtered_image.max() - filtered_image.min())
+    return difference
 
-    plt.imshow(xs)
-    plt.show()
+def scoring_func():
+    x0 = np.array([0.01, 0.01])
+    # A function that implements the CMA-ES algorithm on the noisy dataset to optimize the values of lambda and c
+    #minimization_sol, es = cma.fmin2(image_difference, [0.03, 0.02], 0.5)
+    minimization_sol = optimize.minimize(image_difference, x0, method= "Nelder-Mead")
+    print(minimization_sol)
 
+
+add_noise()
+smoothing_function()
+scoring_func()
