4fb6aa1233dac54c3d45473251c8644c97a8ceaf
TobiasHeOl2 <ohsaibot@mail.com>
2019-11-29 13:52:16 +0000

    :cat:

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import cv2
 from PDEfdsolver import *
-#from PDESolver import *
+from PDESolver import *
 
 
-im = cv2.imread("cat.jpg")
+im = cv2.imread("images/cat.jpg")
 im = im.copy()[125:225, 250:350, :]
 
 dt = 0.1
@@ -13,10 +13,7 @@ added_noise = 100
 
 noise_im = add_noise(im, added_noise)
 
-
-
-
-smoothed_im = fdsolver(noise_im, dt, ld, iterations)
+smoothed_im = fdsolver(noise_im, gfunc, dt, ld, iterations)
 plot_results(im, noise_im, smoothed_im, added_noise)
 
 
